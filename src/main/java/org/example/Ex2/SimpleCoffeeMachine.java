8549c626f93b319755b7d0c510351b9a2bc93452
Serhii Horb <serhii.horb.ua@gmail.com>
2024-02-03 16:15:18 +0100

    home work 5 update

--- src/main/java/org/example/Ex2/SimpleCoffeeMachine.java
+++ src/main/java/org/example/Ex2/SimpleCoffeeMachine.java
@@ -1,1 +1,8 @@
-package org.example.Ex2;public class SimpleCoffeeMachine {    public static void main(String[] args) {      CoffeeMachine coffeeMachine = CoffeeMachine.getPhilips();      coffeeMachine.getCoffee();    }}
+package org.example.Ex2;
+
+public class SimpleCoffeeMachine {
+    public static void main(String[] args) {
+        CoffeeMachine coffeeMachine = CoffeeMachine.getPhilips();
+        coffeeMachine.getCoffee();
+    }
+}
