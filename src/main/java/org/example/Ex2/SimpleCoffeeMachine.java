7d4bf2732837e316decbea7ac972be5f61c71169
Serhii Horb <serhii.horb.ua@gmail.com>
2024-02-03 16:02:17 +0100

    home work 5

---
+++ src/main/java/org/example/Ex2/SimpleCoffeeMachine.java
@@ -0,0 +1,1 @@
+package org.example.Ex2;public class SimpleCoffeeMachine {    public static void main(String[] args) {      CoffeeMachine coffeeMachine = CoffeeMachine.getPhilips();      coffeeMachine.getCoffee();    }}
